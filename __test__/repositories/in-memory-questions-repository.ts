20cf20b3101b857e10e828fca5b28a4cb0db07b1
Matheus Da Mata <matheusdamatag@gmail.com>
2024-08-10 18:38:49 -0300

    feat: add use-case/tests delete-question

--- __test__/repositories/in-memory-questions-repository.ts
+++ __test__/repositories/in-memory-questions-repository.ts
@@ -8,6 +8,22 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     this.items.push(question)
   }
 
+  async delete(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+
+    this.items.splice(itemIndex, 1)
+  }
+
+  async findById(questionId: string) {
+    const question = this.items.find(
+      (item) => item.id.toString() === questionId,
+    )
+
+    if (!question) return null
+
+    return question
+  }
+
   async findBySlug(slug: string) {
     const question = this.items.find((item) => item.slug.value === slug)
 
---
+++ src/domain/forum/application/use-cases/delete-question.spec.ts
@@ -0,0 +1,50 @@
+import { InMemoryQuestionsRepository } from '__test__/repositories/in-memory-questions-repository'
+import { makeQuestion } from '__test__/factories/make-question'
+import { DeleteQuestionUseCase } from './delete-question'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: DeleteQuestionUseCase
+
+describe('Delete Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new DeleteQuestionUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to delete a question', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityID('author-05'),
+      },
+      new UniqueEntityID('question-01'),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    await sut.execute({
+      authorId: 'author-05',
+      questionId: 'question-01',
+    })
+
+    expect(inMemoryQuestionsRepository.items).toHaveLength(0)
+  })
+
+  it('should not be able to delete a question from another user', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityID('author-05'),
+      },
+      new UniqueEntityID('question-01'),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    await expect(() =>
+      sut.execute({
+        authorId: 'author-02',
+        questionId: 'question-01',
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+})
